b9a448c2e8282b7713e6c2fe68675527c8bd65e2
sanusi15 <you@example.com>
2022-11-24 13:36:03 +0700

    Initial and setup Splash Screen

---
+++ android/app/src/main/kotlin/com/skuycode/movie_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.skuycode.movie_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlW500 = 'https://api.themoviedb.org/t/p/w500';
+  static const String imageUrlOriginal = 'https://api.themoviedb.org/t/p/original';
+  static const String apiKey = 'e57382bf1f7f9dcc8b6b8740670b3ffa';
+}
